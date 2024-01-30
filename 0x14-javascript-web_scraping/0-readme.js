6df1f9966a265d935618e7918e5a04b4bae5a7b6
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-01-30 22:04:27 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,23 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-// Check if the file path is provided as a command line argument
-if (process.argv.length < 3) {
-  console.error('Usage: node readfile.js <file_path>');
-  process.exit(1);
-}
-
-// Get the file path from the command line arguments
 const filePath = process.argv[2];
 
-// Read the content of the file in utf-8
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    // Print the error if an error occurred during reading
-    console.error(`Error reading the file: ${err.message}`);
-  } else {
-    // Print the content of the file if read successfully
-    console.log('File content:');
-    console.log(data);
-  }
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) { console.log(error); } else { console.log(data); }
 });
