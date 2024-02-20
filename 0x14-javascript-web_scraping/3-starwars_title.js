644df0eec238911e7c28eaa01e35c82affa828e2
tessykc <tessykc@gmail.com>
2024-02-21 00:11:36 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
+
 // Get the API URL from the command line arguments
 const movieID = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/:id' + movieID
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
 
 request(url, (error, response, body) => {
-  if (error) {console.log(error); }
-    // Print the error if there is an issue with the request
+  if (error) {console.log(error); 
+    return;
+  }
+  // Check if the response status code is not 200 (OK)
+  if (response.statusCode !== 200) {
+    console.log(`Failed to fetch movie data for ID ${movieID}`);
+    return;
+  }
+  try {
     const jsonBody = JSON.parse(body);
     console.log(jsonBody.title);
+  } catch (parseError) {
+    console.log('Error parsing JSON response:', parseError);
   }
-)
+});
+  
