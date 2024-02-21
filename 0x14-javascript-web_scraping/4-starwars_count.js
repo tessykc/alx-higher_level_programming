f320a4fbed656bd783a7006f0ae7f4280b586c76
tessykc <tessykc@gmail.com>
2024-02-21 08:50:27 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,38 @@
+#!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
 
 
 request(url, (error, response, body) => {
-  if (error) { console.log(error);}
+  if (error) { console.log(error); return;}
   const jsonBody = JSON.parse(body);
   let wedgeCount = 0;
-  for (const result of jsonBody.results){
-    for (const charUrl of result.characters){
-      if (charUrl.includes(18)){
+  const fetchCharacterData = (charUrl) => {
+    return new Promise((resolve, reject) => {
+      request(charUrl, (error, response, body) => {
+        if (error) { reject(error); return;}
+        resolve(JSON.parse(body));
+      });
+    });
+  };
+
+  const checkWedgePresence = async (charUrl) => {
+    try {
+      const characterData = await fetchCharacterData(charUrl);
+      if (characterData && characterData.url === 'https://swapi-api.alx-tools.com/api/people/18/') {
         wedgeCount++;
       }
+    } catch (error) {
+      console.error('Error fetching character data:', error);
     }
-  }
-  console.log(wedgeCount);
+  };
+
+  (async () => {
+    for (const result of jsonBody.results) {
+      for (const charUrl of result.characters) {
+        await checkWedgePresence(charUrl);
+      }
+    }
+    console.log('Number of movies with Wedge Antilles:', wedgeCount);
+  })();
 });
