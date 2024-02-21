5192ef413392f5e28a0cb53706d1b79d52b1e6ba
tessykc <tessykc@gmail.com>
2024-02-21 16:08:52 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,38 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const url = 'https://jsonplaceholder.typicode.com/todos' + movieId;
+const apiUrl = 'https://swapi.dev/api/films/' + movieId + '/';
 
-request(url, async(error, response, body) => {
-  if (error) {console.log(error); }
-  const result = JSON.parse(body);
-
-  for (const charUrl, of result.characters){
-    await new Promise((resolve, reject) => {
-      request(charUrl, (err, r, body) => {
-        if (err) {
-          reject(err);
-        } else{
-          console.log(JSON.parse(body).name);
-          resolve();
-        }
-      })
-    })
+request(apiUrl, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
 
-})
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  // Function to fetch character details
+  const fetchCharacterDetails = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+  };
+
+  // Fetch and print character names
+  console.log(`Characters of ${filmData.title}:`);
+  try {
+    for (const charUrl of characters) {
+      const characterName = await fetchCharacterDetails(charUrl);
+      console.log(characterName);
+    }
+  } catch (err) {
+    console.error('Error fetching character details:', err);
+  }
+});
